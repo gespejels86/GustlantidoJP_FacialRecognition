e9eef6a589518f1b1a194b48d473efc699abc7d7
gespejels86 <gespejels@gmail.com>
2018-06-07 20:08:50 -0500

    GuessWho_UserInterface
    User Interfaces for facial recognition project.

---
+++ GuessWhoProject/app/src/main/java/gustlantidojp/guesswho/GuessWho_Main.java
@@ -0,0 +1,13 @@
+package gustlantidojp.guesswho;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuessWho_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.guess_who_layout);
+    }
+}
